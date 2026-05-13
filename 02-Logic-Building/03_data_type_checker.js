bd29b946fe823c2a6ec1180e485bc946da7e2573
akash <akashraval3709@gmail.com>
2026-05-13 18:34:47 +0530

    Add JavaScript data type checker program
    Added a JavaScript program that checks the data type of a variable using switch-case and typeof operator with formatted output messages.

---
+++ 02-Logic-Building/03_data_type_checker.js
@@ -0,0 +1,15 @@
+let value = "10";
+
+switch (typeof value) {
+
+    case "number":
+        console.log("The value is a Number");
+        break;
+
+    case "string":
+        console.log("The value is a String");
+        break;
+
+    default:
+        console.log("Unknown Data Type");
+}
