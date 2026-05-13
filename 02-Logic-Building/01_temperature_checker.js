e42d6ffd5b37f619d4f85009e61311cbd5cb3bf3
akash <akashraval3709@gmail.com>
2026-05-13 18:30:49 +0530

    Added a JavaScript program to check weather conditions based on temperature using if-else statements.
    This file contains a simple JavaScript program that checks temperature conditions and displays weather messages like Hot Weather, Nice Weather, or Cold Weather using if-else statements.

---
+++ 02-Logic-Building/01_temperature_checker.js
@@ -0,0 +1,11 @@
+let temperature = 40;
+
+if (temperature >= 40) {
+    console.log("Hot Weather");
+} 
+else if (temperature >= 20) {
+    console.log("Nice Weather");
+} 
+else {
+    console.log("Cold Weather");
+}
