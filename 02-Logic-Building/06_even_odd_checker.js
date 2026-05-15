918ea4b20b34ea7324b3a0ed3f598918e02b0694
akash <akashraval3709@gmail.com>
2026-05-15 18:29:26 +0530

    Add even odd checker using arrow function
    Added a JavaScript arrow function program to determine whether a number is even or odd using conditional logic and modulus operator.

---
+++ 02-Logic-Building/06_even_odd_checker.js
@@ -0,0 +1,13 @@
+const evenOdd = (num) => {
+
+    if (num % 2 === 0) {
+        return "Even";
+    } 
+    
+    else {
+        return "Odd";
+    }
+};
+
+console.log(evenOdd(34));
+console.log(evenOdd(29));
