e5980da04db9d50b9e55ec16c0fe0921b610eca0
akash <akashraval3709@gmail.com>
2026-06-18 21:21:48 +0530

    Enhance bank login validation logic
    Improved form validation with focus, blur, and input events. Added real-time account number checking, dynamic validation messages, password validation, and login success handling for a better user experience.

--- 03-DOM-Manipulation/bank_login_form_validation/script.js
+++ 03-DOM-Manipulation/bank_login_form_validation/script.js
@@ -2,27 +2,65 @@ const regFrom = document.querySelector('#regFrom')
 const button = document.querySelector('button')
 let login = false
 
-const bNUm=document.querySelector('#bNUm') 
+const bNUm = document.querySelector('#bNUm')
+const password = document.querySelector('#pass')
 
-bNUm.addEventListener('input' , ()=>{
-   
-    let msg = document.querySelector('.msg')
+let alertMsg = document.querySelector('.alret-msg')
+let eMsgPass = document.querySelector('.errorPass')
+let msg = document.querySelector('.msg')
 
-    if(bNUm.value.length < 10)
-    { msg.style.color='red'
-          msg.textContent='Typing... (Must be 10 digits)'
+bNUm.addEventListener('focus', () => {
+    alertMsg.style.backgroundColor = '#dedede';
+
+    alertMsg.textContent = 'Please enter your registered 10-digit account number.'
+
+
+});
+
+
+password.addEventListener('focus', () => {
+    alertMsg.style.display = 'block';
+    alertMsg.textContent = 'Password must include special characters like @, #, $.'
+});
+
+
+
+bNUm.addEventListener('blur', (e) => {
+    if (e.target.value === '') {
+        msg.style.color = 'red'
+        msg.textContent = 'This field is mandatory!'
     }
-    else if(bNUm.value.length == 10){
+});
 
-        msg.style.color='green'
-          msg.textContent='Perfect 10 Digits!'
-    }
-    else{
-         msg.style.color='orangered'
-          msg.textContent='Too long! Exceeded 10 digits!'
+password.addEventListener('blur', (e) => {
+    if (e.target.value === '') {
+        eMsgPass.style.color = 'red'
+        eMsgPass.textContent = 'This field is mandatory!'
     }
 
-})
+});
+
+
+bNUm.addEventListener('input', () => {
+
+
+
+    if (bNUm.value.length < 10) {
+        msg.style.color = 'red'
+        msg.textContent = 'Typing... (Must be 10 digits)'
+    }
+    else if (bNUm.value.length == 10) {
+
+        msg.style.color = 'green'
+        msg.textContent = 'Perfect 10 Digits!'
+    }
+    else {
+        msg.style.color = 'orangered'
+        msg.textContent = 'Too long! Exceeded 10 digits!'
+    }
+
+});
+
 
 regFrom.addEventListener('submit', (e) => {
     let accnum = false
@@ -31,7 +69,7 @@ regFrom.addEventListener('submit', (e) => {
 
     const accNum = document.querySelector('#bNUm').value.trim()
     const eMsg = document.querySelector('.errorMsg')
-    const eMsgPass = document.querySelector('.errorPass')
+
     const button = document.querySelector('button')
     const password = document.querySelector('#pass').value.trim()
 
@@ -77,6 +115,14 @@ regFrom.addEventListener('submit', (e) => {
     if (accnum && pass) {
         button.disabled = true
         login = true
+        regFrom.reset();
+        alertMsg.style.backgroundColor = ''
+        eMsg.textContent = '';
+        alertMsg.textContent = '';
+        msg.textContent = '';
+        eMsgPass.textContent = ''
+        alertMsg.style.display = 'none';
+
 
         if (login) {
             setTimeout(() => {
