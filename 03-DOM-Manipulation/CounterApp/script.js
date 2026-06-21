7898c92efcd5caca4976e58b3af4b681fb3fec2d
akash <akashraval3709@gmail.com>
2026-06-21 18:39:50 +0530

    Add localStorage support to Counter App
    Enhanced the Counter App by storing counter values in localStorage, allowing data persistence across page reloads. Added dynamic color changes for increase, decrease, and reset actions to improve user feedback and interaction.

--- 03-DOM-Manipulation/CounterApp/script.js
+++ 03-DOM-Manipulation/CounterApp/script.js
@@ -3,24 +3,51 @@ const decreaseButton = document.querySelector('.de-btn')
 const resetButton = document.querySelector('.re-btn')
 const count = document.querySelector('.count')
 
-let num = 0
 
+let num = 0
 increaseButton.addEventListener('click' , ()=>{
     num+=1
+    count.style.color='green'
     count.textContent = num
+    
+    localStorage.setItem('num',num)
+    
+    
 })
 
-decreaseButton.addEventListener('click' , () => {
-    if(num > 0){
 
+decreaseButton.addEventListener('click' , () => {
+    
+    if(num > 0){
+        
+        
         num-=1
+        count.style.color='red'
         count.textContent = num
+    localStorage.setItem('num',num)
     }
+    
 })
 
 resetButton.addEventListener('click' ,() => {
     num=0
+    localStorage.setItem('num',num)
+    count.style.color='orange'
     count.textContent = num
+   localStorage.setItem('num',num)
 })
 
 
+
+function getitem(){
+    
+    count.textContent=localStorage.getItem('num')
+    num=Number(count.textContent)
+    console.log(num)
+    
+}
+
+
+    
+
+getitem()
