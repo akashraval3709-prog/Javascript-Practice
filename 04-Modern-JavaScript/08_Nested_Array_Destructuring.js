deb34ca5e7da9c20758decd2afeb25a177c8e310
akash <akashraval3709@gmail.com>
2026-07-07 20:54:44 +0530

    feat: add nested array destructuring example
    Added a JavaScript example demonstrating nested array destructuring in ES6. The program extracts values from nested arrays, skips unnecessary elements, and accesses specific data using concise destructuring syntax.

---
+++ 04-Modern-JavaScript/08_Nested_Array_Destructuring.js
@@ -0,0 +1,10 @@
+const blockchainTx = [ "TX-99021", ["0xAkashAddress...", "0xReceiverAddress..."], 5.45, "Success" ];
+
+
+
+const [txId,[sender,],cryptoAmount,] =blockchainTx
+
+console.log(txId);
+console.log(sender);
+console.log(cryptoAmount);
+
