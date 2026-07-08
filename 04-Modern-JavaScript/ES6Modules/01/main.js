d1806dd2697006f6b71fd51f0bc7257814f6cb8a
akash <akashraval3709@gmail.com>
2026-07-08 22:16:09 +0530

    Add ES6 module import example
    Implemented ES6 module imports using both named and default imports. Demonstrated how to access exported variables and functions from another module.

---
+++ 04-Modern-JavaScript/ES6Modules/01/main.js
@@ -0,0 +1,12 @@
+import keys from './cyberSecurity.js'
+
+import { encryptData,decryptData,key } from './cyberSecurity.js'
+
+let e=encryptData()
+let d=decryptData()
+console.log(key);
+console.log(keys);
+console.log(e);
+console.log(d);
+
+
